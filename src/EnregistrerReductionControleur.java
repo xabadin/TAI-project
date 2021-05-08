44ba605e2aca161ef7961c762019a0e711a76f4d
xabadin <80093086+xabadin@users.noreply.github.com>
2021-05-08 23:25:20 +0200

    finition vérif ajout reduc

--- src/EnregistrerReductionControleur.java
+++ src/EnregistrerReductionControleur.java
@@ -71,62 +71,73 @@ public class EnregistrerReductionControleur extends HttpServlet {
 			request.setAttribute("validation", validation);
 		}
 		else {
-			
-			reduction.setMax(Integer.parseInt(request.getParameter("max1")));
-			reduction.setMin(Integer.parseInt(request.getParameter("min1")));
-			reduction.setReduc(Double.parseDouble(request.getParameter("reduc1")));
-			reduction.setProduit(produit);
-			
-			reductionDAOModele.creer(reduction);
-			String validation = "La réduction a été enregistrée.";
-			request.setAttribute("validation", validation);
-			
-			if(request.getParameter("min2") == "" || request.getParameter("max2") == "" || request.getParameter("reduc2") == "")
+			if(Integer.parseInt(request.getParameter("max1")) < (Integer.parseInt(request.getParameter("min1"))))
 			{
+				String validation = "Le max doit être supérieur au min.";
+				request.setAttribute("validation", validation);
+			}
+			else {
+				reduction.setMax(Integer.parseInt(request.getParameter("max1")));
+				reduction.setMin(Integer.parseInt(request.getParameter("min1")));
+				reduction.setReduc(Double.parseDouble(request.getParameter("reduc1")));
+				reduction.setProduit(produit);
 				
-			}else {
+				reductionDAOModele.creer(reduction);
+				String validation = "La réduction a été enregistrée.";
+				request.setAttribute("validation", validation);
 				
-				reduction1.setMax(Integer.parseInt(request.getParameter("max2")));
-				reduction1.setMin(Integer.parseInt(request.getParameter("min2")));
-				reduction1.setReduc(Double.parseDouble(request.getParameter("reduc2")));
-				reduction1.setProduit(produit);
-				
-				reductionDAOModele.creer(reduction1);
-				
-				if(request.getParameter("min3") == "" || request.getParameter("max3") == "" || request.getParameter("reduc3") == "")
+				if(request.getParameter("min2") == "" || request.getParameter("max2") == "" || request.getParameter("reduc2") == "")
 				{
 				}else {
-					reduction2.setMax(Integer.parseInt(request.getParameter("max3")));
-					reduction2.setMin(Integer.parseInt(request.getParameter("min3")));
-					reduction2.setReduc(Double.parseDouble(request.getParameter("reduc3")));
-					reduction2.setProduit(produit);
 					
-					reductionDAOModele.creer(reduction2);
-					if(request.getParameter("min4") == "" || request.getParameter("max4") == "" || request.getParameter("reduc4") == "")
+					if(Integer.parseInt(request.getParameter("max2")) < (Integer.parseInt(request.getParameter("min2"))) || Integer.parseInt(request.getParameter("min2")) < Integer.parseInt(request.getParameter("max1")))
 					{
-					}else {
-						reduction3.setMax(Integer.parseInt(request.getParameter("max4")));
-						reduction3.setMin(Integer.parseInt(request.getParameter("min4")));
-						reduction3.setReduc(Double.parseDouble(request.getParameter("reduc4")));
-						reduction3.setProduit(produit);
+						String validation1 = "Le max doit être supérieur au min. Si plusieurs lignes sont remplis le max doit être égal ou inférieur au min suivant";
+						request.setAttribute("validation", validation1);
+					}
+					else {
+						reduction1.setMax(Integer.parseInt(request.getParameter("max2")));
+						reduction1.setMin(Integer.parseInt(request.getParameter("min2")));
+						reduction1.setReduc(Double.parseDouble(request.getParameter("reduc2")));
+						reduction1.setProduit(produit);
 						
-						reductionDAOModele.creer(reduction3);
-						if(request.getParameter("min5") == "" || request.getParameter("max5") == "" || request.getParameter("reduc5") == "")
+						reductionDAOModele.creer(reduction1);
+						
+						if(request.getParameter("min3") == "" || request.getParameter("max3") == "" || request.getParameter("reduc3") == "")
 						{
 						}else {
-							reduction4.setMax(Integer.parseInt(request.getParameter("max5")));
-							reduction4.setMin(Integer.parseInt(request.getParameter("min5")));
-							reduction4.setReduc(Double.parseDouble(request.getParameter("reduc5")));
-							reduction4.setProduit(produit);
+							reduction2.setMax(Integer.parseInt(request.getParameter("max3")));
+							reduction2.setMin(Integer.parseInt(request.getParameter("min3")));
+							reduction2.setReduc(Double.parseDouble(request.getParameter("reduc3")));
+							reduction2.setProduit(produit);
 							
-							reductionDAOModele.creer(reduction4);
+							reductionDAOModele.creer(reduction2);
+							if(request.getParameter("min4") == "" || request.getParameter("max4") == "" || request.getParameter("reduc4") == "")
+							{
+							}else {
+								reduction3.setMax(Integer.parseInt(request.getParameter("max4")));
+								reduction3.setMin(Integer.parseInt(request.getParameter("min4")));
+								reduction3.setReduc(Double.parseDouble(request.getParameter("reduc4")));
+								reduction3.setProduit(produit);
+								
+								reductionDAOModele.creer(reduction3);
+								if(request.getParameter("min5") == "" || request.getParameter("max5") == "" || request.getParameter("reduc5") == "")
+								{
+								}else {
+									reduction4.setMax(Integer.parseInt(request.getParameter("max5")));
+									reduction4.setMin(Integer.parseInt(request.getParameter("min5")));
+									reduction4.setReduc(Double.parseDouble(request.getParameter("reduc5")));
+									reduction4.setProduit(produit);
+									
+									reductionDAOModele.creer(reduction4);
+								}
+							}
 						}
 					}
 				}
 			}
 		}
 		request.setAttribute("produitListe", produitListe);
-		
 		request.getRequestDispatcher("/enregistrerReductionVue.jsp").forward(request, response);
 	}
 
